c06390d5082a7ea79b7bd69b9623f7b95517a5ff
jaydeep apte <jaydeepresources@gmail.com>
2018-04-15 10:45:18 +0530

    Add files via upload

--- node-express-web-service/restful.js
+++ node-express-web-service/restful.js
@@ -2,7 +2,8 @@ var express = require('express'),
   bodyParser = require('body-parser');
 
 var app = express();
-
+var cors = require('cors');
+app.use(cors());
 app.use(bodyParser.json());
 
 var users = [
@@ -15,17 +16,18 @@ app.get('/listUsers', function (req, res) {
   res.end(JSON.stringify(users));
 });
 
-app.put('/edituser', function (req, res) {
+app.put('/editUser', function (req, res) {
   res.end('http put')
 });
 
-app.post('/adduser', function (req, res) {
+app.post('/addUser', function (req, res) {
   users.push(req.body);
   res.end(JSON.stringify(users));
 });
 
-app.delete('/deleteusers', function (req, res) {
-  res.end('http delete')
+app.delete('/deleteUser/:id', function (req, res) {  
+  users.splice(req.params.id,1);
+  res.end(JSON.stringify(users));
 })
 
 var server = app.listen(8081, function () {
